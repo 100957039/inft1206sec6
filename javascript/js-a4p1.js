b2dea7a2d0798f8743832ed8d473ab28a2464e71
100957039 <rachel.boden@dcmail.ca>
2024-07-20 19:41:12 -0400

    Updated a4p1.html & js-a4p1.js version 3
    Added variables to hold the story and values for randomization.  Added replaceAll functions to alter the story text to the randomized values.

--- javascript/js-a4p1.js
+++ javascript/js-a4p1.js
@@ -9,16 +9,29 @@ function randomValueFromArray(array){
   return array[random];
 }
 
+// RAW TEXT STRINGS
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 // EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
 function result() {
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:', xItem);
+  newStory = newStory.replaceAll(':inserty:', yItem);
+  newStory = newStory.replaceAll(':insertz:', zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory.replaceAll('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
